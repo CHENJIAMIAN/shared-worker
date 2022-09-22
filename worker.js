ad40e805e3546af02fb344bcef36593645ef82d2
cjm <571574085@qq.com>
2022-09-22 21:23:07 +0800

    m

---
+++ worker.js
@@ -0,0 +1,20 @@
+onconnect = (e)=>{
+    var port = e.ports[0];
+
+    port.onmessage=(ee)=>{
+        console.log(`worker.js onmessage`,ee)
+        port.postMessage('worker postMessage');
+    }
+
+}
+
+// onconnect = function(e) {
+//     var port = e.ports[0];
+
+//     port.addEventListener('message', function(e) {
+//       var workerResult = 'Result: ' + (e.data[0] * e.data[1]);
+//       port.postMessage(workerResult);
+//     });
+
+//     port.start(); // Required when using addEventListener. Otherwise called implicitly by onmessage setter.
+// }
